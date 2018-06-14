551ce3f224f0f9d4b96121e700d58c8e8d930575
Tadatsune <iidatadatsune@gmail.com>
2018-06-14 13:15:35 +0900

    20180614 UpDate

--- FindStringPattern.m
+++
@@ -1,8 +0,0 @@
-function containPattern = FindStringPattern(stringCell,stringPattern)
-%*****************************************************************************
-%This function uses "stringCell", cell array of strings, and "stringPattern",
-%string pattern to find. The output is "conteinPattern", 1 or 0.
-%*****************************************************************************
-
-x = strfind(stringCell,stringPattern,'ForceCellOutput',true);
-containPattern = not(cellfun('isempty',x));
